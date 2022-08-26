1d89931c9e58d849c540b20f09bac185c9c3f5af
Bach Vo <bach_vo@hotmail.com>
2022-08-25 21:45:19 -0700

    initialize react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FamilyChart from './components/family-chart';
+
+function App() {
+  return (
+    <div className="App">
+      <FamilyChart/>
+    </div>
+  );
+}
+
+export default App;
