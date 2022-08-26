866e85524be133a447e4ed1a28ea50b1a5cba9aa
Bach Vo <bach_vo@hotmail.com>
2022-08-26 04:25:35 -0700

    bring back app test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import FamilyChart from './components/family-chart';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="app">
       <FamilyChart/>
     </div>
   );
